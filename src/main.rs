4f9f53be5680eca582d7c9c4b35eb11d15b8337c
Elioenai Pinheiro Ferrari <elioenai.ferrari@ifood.com.br>
2024-10-28 11:00:52 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+mod models;
+use models::*;
+
+use actix_web::{web::Data, App, HttpServer};
+
+#[derive(Debug, Clone)]
+struct State {
+    pub movies: Vec<Movie>,
+    pub series: Vec<Serie>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let series = vec![
+        Serie{
+            id: "1".to_string(),
+            title: "Peak Blinders".to_string(),
+            thumbnail_url:"https://oldsiller.com.br/cdn/shop/articles/Estilo_masculino_peaky_blinders.jpg?v=1687810856".to_string(),
+            description: "Peak Blinders é uma série de televisão britânica de drama histórico sobre uma família de gângsteres pós Primeira Guerra Mundial, que é chefiada por Tommy Shelby (Cillian Murphy).".to_string(),
+            genre: "Ação".to_string(),
+            seasons: vec![
+                Season{
+                    id: "1".to_string(),
+                    serie_id: "1".to_string(),
+                    number: 1,
+                    serie: None,
+                    episodes: vec![
+                        Episode{
+                            id: "1".to_string(),
+                            season_id: "1".to_string(),
+                            number: 1,
+                            title: "Piloto".to_string(),
+                            description: "Os Vingadores se reúnem para desfazer as ações de Thanos e restaurar o universo.".to_string(),
+                            video_url: "https://www.youtube.com/watch?v=5TbW4Zp8Z2A".to_string(),
+                            thumbnail_url: "https://tm.ibxk.com.br/2022/03/07/07013050568001.jpg".to_string(),
+                            duration: 180,
+                            mime_type: "video/mp4".to_string(),
+                            season: None,
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    let movies = vec![
+        Movie{
+            id: "1".to_string(),
+            title: "Vingadores - Ultimato".to_string(),
+            thumbnail_url:"https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/BF0D/production/_106090984_2e39b218-c369-452e-b5be-d2476f9d8728.jpg.webp".to_string(),
+            description: "Os Vingadores se reúnem para desfazer as ações de Thanos e restaurar o universo.".to_string(),
+            video_url: "https://www.youtube.com/watch?v=5TbW4Zp8Z2A".to_string(),
+            duration: 180,
+            mime_type: "video/mp4".to_string(),
+            genre: "Ação".to_string(),
+        }
+    ];
+
+    let state = State { movies, series };
+    HttpServer::new(move || App::new().app_data(Data::new(state.clone())))
+        .bind(("0.0.0.0", 4000))?
+        .run()
+        .await
+}
---
+++ src/models/mod.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize,Clone)]
+pub struct Movie {
+    pub id: String,
+    pub title: String,
+    pub description: String,
+    pub video_url: String,
+    pub thumbnail_url: String,
+    pub duration: i32,
+    pub mime_type: String,
+    pub genre: String,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone)]
+pub struct Serie {
+    pub id: String,
+    pub title: String,
+    pub description: String,
+    pub thumbnail_url: String,
+    pub genre: String,
+    pub seasons: Vec<Season>,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone)]
+pub struct Season {
+    pub id: String,
+    pub serie_id: String,
+    pub serie: Option<Serie>,
+    pub episodes: Vec<Episode>,
+    pub number: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize,Clone)]
+pub struct Episode {
+    pub id: String,
+    pub season_id: String,
+    pub season: Option<Season>,
+    pub number: i32,
+    pub title: String,
+    pub description: String,
+    pub video_url: String,
+    pub thumbnail_url: String,
+    pub duration: i32,
+    pub mime_type: String,
+}
